94e3f3c325dfdb462fea54d7a27438e08e103531
Calfson <162500017+Calfson@users.noreply.github.com>
2024-03-08 14:59:35 -0500

    Delete Diseño-html/Diseño-html directory

--- Diseño-html/Diseño-html/Nav.js
+++
@@ -1,9 +0,0 @@
-function showForm(formId) {
-    var forms = document.querySelectorAll('.formularios > div');
-    forms.forEach(function(form) {
-        form.classList.remove('show');
-    });
-
-    var formToShow = document.getElementById(formId);
-    formToShow.classList.add('show');
-}
